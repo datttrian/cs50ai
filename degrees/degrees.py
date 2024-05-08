f72009756fe4a5128dcab3ac4ad81a1d966b4a8b
datttrian <datttrian@gmail.com>
2024-05-08 09:09:40 +0000

    Wed, May 8, 2024, 9:09 AM +00:00

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -93,7 +93,28 @@ def shortest_path(source, target):
     """
 
     # TODO
-    raise NotImplementedError
+    # raise NotImplementedError
+    start = Node(state=source, parent=None, action=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+    explored = set()
+    while True:
+        if frontier.empty():
+            return None
+        node = frontier.remove()
+        explored.add(node.state)
+
+        for movie_id, person_id in neighbors_for_person(node.state):
+            if not frontier.contains_state(person_id) and person_id not in explored:
+                child = Node(state=person_id, parent=node, action=movie_id)
+                if person_id == target:
+                    path = []
+                    while child.parent is not None:
+                        path.append((child.action, child.state))
+                        child = child.parent
+                    path.reverse()
+                    return path
+                frontier.add(child)
 
 
 def person_id_for_name(name):
