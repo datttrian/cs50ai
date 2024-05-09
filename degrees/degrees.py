8d570163205e77d0cb4defe00ffa1aba937d4615
datttrian <datttrian@gmail.com>
2024-05-09 08:30:59 +0000

    Thu, May 9, 2024, 8:30 AM +00:00

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -18,6 +18,18 @@ def load_data(directory):
     Load data from CSV files into memory.
     """
     # Load people
+    with open(f"{directory}/people.csv", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            people[row["id"]] = {
+                "name": row["name"],
+                "birth": row["birth"],
+                "movies": set(),
+            }
+            if row["name"].lower() not in names:
+                names[row["name"].lower()] = {row["id"]}
+            else:
+                names[row["name"].lower()].add(row["id"])
 
     # Load movies
 
