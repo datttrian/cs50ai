6ff2bc8e22af234a0418a9f72ed44cdafc0cc14b
datttrian <datttrian@gmail.com>
2024-05-10 08:02:01 +0000

    Fri, May 10, 2024, 8:02 AM +00:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -13,114 +13,55 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    num_X = sum(row.count(X) for row in board)
-    num_O = sum(row.count(O) for row in board)
-    return X if num_X == num_O else O
+    raise NotImplementedError
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    possible_actions = set()
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] is EMPTY:
-                possible_actions.add((i, j))
-    return possible_actions
+    raise NotImplementedError
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    i, j = action
-    current_player = player(board)
-    if board[i][j] is not EMPTY:
-        raise Exception("Invalid action")
-    new_board = [row[:] for row in board]
-    new_board[i][j] = current_player
-    return new_board
+    raise NotImplementedError
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    # Check rows
-    for row in board:
-        if row[0] == row[1] == row[2] != EMPTY:
-            return row[0]
-
-    # Check columns
-    for j in range(3):
-        if board[0][j] == board[1][j] == board[2][j] != EMPTY:
-            return board[0][j]
-
-    # Check diagonals
-    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
-        return board[0][0]
-    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
-        return board[0][2]
-
-    return None
+    raise NotImplementedError
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    return winner(board) is not None or all(
-        all(cell is not EMPTY for cell in row) for row in board
-    )
+    raise NotImplementedError
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    winner_player = winner(board)
-    if winner_player == X:
-        return 1
-    elif winner_player == O:
-        return -1
-    else:
-        return 0
+    raise NotImplementedError
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    current_player = player(board)
-
-    if terminal(board):
-        return None
-
-    def max_value(board):
-        if terminal(board):
-            return utility(board)
-        v = float("-inf")
-        for action in actions(board):
-            v = max(v, min_value(result(board, action)))
-        return v
-
-    def min_value(board):
-        if terminal(board):
-            return utility(board)
-        v = float("inf")
-        for action in actions(board):
-            v = min(v, max_value(result(board, action)))
-        return v
-
-    if current_player == X:
-        return max(actions(board), key=lambda action: min_value(result(board, action)))
-    else:
-        return min(actions(board), key=lambda action: max_value(result(board, action)))
+    raise NotImplementedError
