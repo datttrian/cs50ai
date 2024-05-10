7faf1a2cdcf80aafc1f62723b691a8bb7762a138
datttrian <datttrian@gmail.com>
2024-05-10 08:16:43 +0000

    Fri, May 10, 2024, 8:16 AM +00:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -57,7 +57,23 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    # Check rows
+    for row in board:
+        if row[0] == row[1] == row[2] != EMPTY:
+            return row[0]
+
+    # Check columns
+    for j in range(3):
+        if board[0][j] == board[1][j] == board[2][j] != EMPTY:
+            return board[0][j]
+
+    # Check diagonals
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+
+    return None
 
 
 def terminal(board):
