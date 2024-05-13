af5a2643a7503f6ed8813c5b698b0b5aba8f133f
datttrian <datttrian@gmail.com>
2024-05-13 09:21:55 +0000

    Mon, May 13, 2024, 9:21 AM +00:00

--- knights/logic.py
+++ knights/logic.py
@@ -12,31 +12,6 @@ class Sentence:
         if not isinstance(sentence, Sentence):
             raise TypeError("must be a logical sentence")
 
-    @classmethod
-    def parenthesize(cls, s):
-        """Parenthesize an expression if not already parenthesized."""
-
-        def balanced(s):
-            """Checks if a string has balanced parentheses."""
-            count = 0
-            for c in s:
-                if c == "(":
-                    count += 1
-                elif c == ")":
-                    if count <= 0:
-                        return False
-                    count -= 1
-            return count == 0
-
-        if (
-            not len(s)
-            or s.isalpha()
-            or (s[0] == "(" and s[-1] == ")" and balanced(s[1:-1]))
-        ):
-            return s
-        else:
-            return f"({s})"
-
 
 class Symbol(Sentence):
     def __init__(self, name):
@@ -48,7 +23,7 @@ class Symbol(Sentence):
     def evaluate(self, model):
         try:
             return bool(model[self.name])
-        except KeyError:
+        except keyError:
             raise Exception(f"variable {self.name} not in model")
 
     def symbols(self):
@@ -65,7 +40,6 @@ class Not(Sentence):
 
     def symbols(self):
         return self.operand.symbols()
-
 
 class And(Sentence):
     def __init__(self, *conjuncts):
@@ -84,58 +58,4 @@ class Or(Sentence):
     def __init__(self, *disjuncts):
         for disjunct in disjuncts:
             Sentence.validate(disjunct)
-        self.disjuncts = list(disjuncts)
-
-    def evaluate(self, model):
-        return any(disjunct.evaluate(model) for disjunct in self.disjuncts)
-
-    def symbols(self):
-        return set.union(*[disjunct.symbols() for disjunct in self.disjuncts])
-
-
-class Implication(Sentence):
-    def __init__(self, antecedent, consequent):
-        Sentence.validate(antecedent)
-        Sentence.validate(consequent)
-        self.antecedent = antecedent
-        self.consequent = consequent
-
-    def evaluate(self, model):
-        return (not self.antecedent.evaluate(model)) or self.consequent.evaluate(model)
-
-
-def model_check(knowledge, query):
-    """Checks if knowledge base entails query."""
-
-    def check_all(knowledge, query, symbols, model):
-        """Checks if knowledge base entails query, given a particular model."""
-
-        # If model has an assignment for each symbol
-        if not symbols:
-            # If knowledge base is true in model, then query must also be true
-            if knowledge.evaluate(model):
-                return query.evaluate(model)
-            return True
-        else:
-            # Choose one of the remaining unused symbols
-            remaining = symbols.copy()
-            p = remaining.pop()
-
-            # Create a model where the symbol is true
-            model_true = model.copy()
-            model_true[p] = True
-
-            # Create a model where the symbol is false
-            model_false = model.copy()
-            model_false[p] = False
-
-            # Ensure entailment holds in both models
-            return check_all(knowledge, query, remaining, model_true) and check_all(
-                knowledge, query, remaining, model_false
-            )
-
-    # Get all symbols in both knowledge and query
-    symbols = set.union(knowledge.symbols(), query.symbols())
-
-    # Check that knowledge entails query
-    return check_all(knowledge, query, symbols, dict())
+            self.dusjuncts = list(disjuncts)
