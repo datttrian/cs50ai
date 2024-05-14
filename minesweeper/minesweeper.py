11034f5890296d5d7d35fe0920030cf0c08b9ae8
Dat Thanh Tran <datttrian@gmail.com>
2024-05-14 08:08:14 +0000

    Tue, May 14, 2024, 8:08 AM +00:00

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -112,21 +112,26 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells
+        return set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
+            self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 
 class MinesweeperAI():
