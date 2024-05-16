6efba1916a0b4e756434e97b1487bc83c2e6a55b
datttrian <datttrian@gmail.com>
2024-05-16 09:00:27 +0000

    Thu, May 16, 2024, 9:00 AM +00:00

--- heredity/heredity.py
+++ heredity/heredity.py
@@ -43,7 +43,43 @@ def main():
     if len(sys.argv) != 2:
         sys.exit("Usage: python heredity.py data.csv")
     people = load_data(sys.argv[1])
-    print(people)
+
+    # Keep track of gene and trait probabilities for each person
+    probabilities = {
+        person: {
+            "gene": {
+                2: 0,
+                1: 0,
+                0: 0
+            },
+            "trait": {
+                True: 0,
+                False: 0
+            }
+        }
+        for person in people
+    }
+
+    # Loop over all sets of people who might have the trait
+    names = set(people)
+    for have_trait in powerset(names):
+
+        # Check if current set of people violates known information
+        fails_evidence = any(
+            (people[person]["trait"] is not None and
+             people[person]["trait"] != (person in have_trait))
+            for person in names
+        )
+        if fails_evidence:
+            continue
+
+        # Loop over all sets of people who might have the gene
+        for one_gene in powerset(names):
+            for two_genes in powerset(names - one_gene):
+
+                # Update probabilities with new joint probability
+                p = joint_probability(people, one_gene, two_genes, have_trait)
+                update(probabilities, one_gene, two_genes, have_trait, p)
 
 
 def load_data(filename):
@@ -66,6 +102,107 @@ def load_data(filename):
                           False if row["trait"] == "0" else None)
             }
     return data
+
+
+def powerset(s):
+    """
+    Return a list of all possible subsets of set s.
+    """
+    s = list(s)
+    return [
+        set(s) for s in itertools.chain.from_iterable(
+            itertools.combinations(s, r) for r in range(len(s) + 1)
+        )
+    ]
+
+
+def joint_probability(people, one_gene, two_genes, have_trait):
+    """
+    Compute and return a joint probability.
+
+    The probability returned should be the probability that
+        * everyone in set `one_gene` has one copy of the gene, and
+        * everyone in set `two_genes` has two copies of the gene, and
+        * everyone not in `one_gene` or `two_gene` does not have the gene, and
+        * everyone in set `have_trait` has the trait, and
+        * everyone not in set` have_trait` does not have the trait.
+    """
+    probability = 1
+
+    for person in people:
+        mother = people[person]['mother']
+        father = people[person]['father']
+
+        if person in two_genes:
+            genes = 2
+        elif person in one_gene:
+            genes = 1
+        else:
+            genes = 0
+
+        if person in have_trait:
+            trait = True
+        else:
+            trait = False
+
+        if mother is None and father is None:
+            probability *= PROBS['gene'][genes]
+        else:
+            if genes == 2:
+                prob_mother = get_gene_probability(mother, True, one_gene, two_genes)
+                prob_father = get_gene_probability(father, True, one_gene, two_genes)
+                probability *= prob_mother * prob_father
+            elif genes == 1:
+                prob_mother = get_gene_probability(mother, True, one_gene, two_genes)
+                prob_father = get_gene_probability(father, False, one_gene, two_genes)
+                probability *= prob_mother * prob_father + (1 - prob_mother) * (1 - prob_father)
+            else:
+                prob_mother = get_gene_probability(mother, False, one_gene, two_genes)
+                prob_father = get_gene_probability(father, False, one_gene, two_genes)
+                probability *= prob_mother * prob_father
+
+        probability *= PROBS['trait'][genes][trait]
+
+    return probability
+
+
+def get_gene_probability(parent, has_gene, one_gene, two_genes):
+    """
+    Helper function to get the probability of passing the gene from parent to child.
+    """
+    if parent in two_genes:
+        if has_gene:
+            return 1 - PROBS['mutation']
+        else:
+            return PROBS['mutation']
+    elif parent in one_gene:
+        return 0.5
+    else:
+        if has_gene:
+            return PROBS['mutation']
+        else:
+            return 1 - PROBS['mutation']
+
+
+def update(probabilities, one_gene, two_genes, have_trait, p):
+    """
+    Add to `probabilities` a new joint probability `p`.
+    Each person should have their "gene" and "trait" distributions updated.
+    Which value for each distribution is updated depends on whether
+    the person is in `have_gene` and `have_trait`, respectively.
+    """
+    for person in probabilities:
+        if person in one_gene:
+            probabilities[person]["gene"][1] += p
+        elif person in two_genes:
+            probabilities[person]["gene"][2] += p
+        else:
+            probabilities[person]["gene"][0] += p
+
+        if person in have_trait:
+            probabilities[person]["trait"][True] += p
+        else:
+            probabilities[person]["trait"][False] += p
 
 
 if __name__ == "__main__":
