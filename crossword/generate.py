6212e80f7e14b886591df7e2b6e42bd30f39ffc4
Dat Thanh Tran <datttrian@gmail.com>
2024-05-17 08:23:21 +0000

    Fri, May 17, 2024, 8:23 AM +00:00

--- crossword/generate.py
+++ crossword/generate.py
@@ -99,7 +99,10 @@ class CrosswordCreator():
         (Remove any values that are inconsistent with a variable's unary
          constraints; in this case, the length of the word.)
         """
-        raise NotImplementedError
+        for var in self.domains:
+            for word in set(self.domains[var]):
+                if len(word) != var.length:
+                    self.domains[var].remove(word)
 
     def revise(self, x, y):
         """
