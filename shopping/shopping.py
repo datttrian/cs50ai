7d1d23eaaabd549e061907b026972ebfb9b31ffa
Dat Tran <datttrian@gmail.com>
2024-05-23 08:21:38 +0000

    Thu, May 23, 2024, 8:21 AM +00:00

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -99,7 +99,9 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    model = KNeighborsClassifier(n_neighbors=1)
+    model.fit(evidence, labels)
+    return model
 
 
 def evaluate(labels, predictions):
