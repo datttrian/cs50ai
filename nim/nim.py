d3a7d4885dd7a0a5247054373c7a02caae03afcd
Dat Thanh Tran <datttrian@gmail.com>
2024-05-24 08:19:20 +0000

    Fri, May 24, 2024, 8:19 AM +00:00

--- nim/nim.py
+++ nim/nim.py
@@ -69,3 +69,117 @@ class Nim:
         if all(pile == 0 for pile in self.piles):
             self.winner = self.player
 
+
+def train(n):
+    """
+    Train an AI by playing `n` games against itself.
+    """
+
+    player = NimAI()
+
+    # Play n games
+    for i in range(n):
+        print(f"Playing training game {i + 1}")
+        game = Nim()
+
+        # Keep track of last move made by either player
+        last = {
+            0: {"state": None, "action": None},
+            1: {"state": None, "action": None}
+        }
+
+        # Game loop
+        while True:
+
+            # Keep track of current state and action
+            state = game.piles.copy()
+            action = player.choose_action(game.piles)
+
+            # Keep track of last state and action
+            last[game.player]["state"] = state
+            last[game.player]["action"] = action
+
+            # Make move
+            game.move(action)
+            new_state = game.piles.copy()
+
+            # When game is over, update Q values with rewards
+            if game.winner is not None:
+                player.update(state, action, new_state, -1)
+                player.update(
+                    last[game.player]["state"],
+                    last[game.player]["action"],
+                    new_state,
+                    1
+                )
+                break
+
+            # If game is continuing, no rewards yet
+            elif last[game.player]["state"] is not None:
+                player.update(
+                    last[game.player]["state"],
+                    last[game.player]["action"],
+                    new_state,
+                    0
+                )
+
+    print("Done training")
+
+    # Return the trained AI
+    return player
+
+
+def play(ai, human_player=None):
+    """
+    Play human game against the AI.
+    `human_player` can be set to 0 or 1 to specify whether
+    human player moves first or second.
+    """
+
+    # If no player order set, choose human's order randomly
+    if human_player is None:
+        human_player = random.randint(0, 1)
+
+    # Create new game
+    game = Nim()
+
+    # Game loop
+    while True:
+
+        # Print contents of piles
+        print()
+        print("Piles:")
+        for i, pile in enumerate(game.piles):
+            print(f"Pile {i}: {pile}")
+        print()
+
+        # Compute available actions
+        available_actions = Nim.available_actions(game.piles)
+        time.sleep(1)
+
+        # Let human make a move
+        if game.player == human_player:
+            print("Your Turn")
+            while True:
+                pile = int(input("Choose Pile: "))
+                count = int(input("Choose Count: "))
+                if (pile, count) in available_actions:
+                    break
+                print("Invalid move, try again.")
+
+        # Have AI make a move
+        else:
+            print("AI's Turn")
+            pile, count = ai.choose_action(game.piles, epsilon=False)
+            print(f"AI chose to take {count} from pile {pile}.")
+
+        # Make move
+        game.move((pile, count))
+
+        # Check for winner
+        if game.winner is not None:
+            print()
+            print("GAME OVER")
+            winner = "Human" if game.winner == human_player else "AI"
+            print(f"Winner is {winner}")
+            return
