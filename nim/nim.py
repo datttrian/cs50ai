201936c02eb98d3b72f8878b457c07903f591512
Dat Thanh Tran <datttrian@gmail.com>
2024-05-24 09:31:26 +0000

    Fri, May 24, 2024, 9:31 AM +00:00

--- nim/nim.py
+++ nim/nim.py
@@ -238,4 +238,57 @@ def train(n):
     return player
 
 
-ai = train(10)
+def play(ai, human_player=None):
+    """
+    Play human game against the AI.
+    `human_player` can be set to 0 or 1 to specify whether
+    human player moves first or second.
+    """
+
+    # If no player order set, choose human's order randomly
+    if human_player is None:
+        human_player = random.randint(0, 1)
+
+    # Create new game
+    game = Nim()
+
+    # Game loop
+    while True:
+
+        # Print contents of piles
+        print()
+        print("Piles:")
+        for i, pile in enumerate(game.piles):
+            print(f"Pile {i}: {pile}")
+        print()
+
+        # Compute available actions
+        available_actions = Nim.available_actions(game.piles)
+        time.sleep(1)
+
+        # Let human make a move
+        if game.player == human_player:
+            print("Your Turn")
+            while True:
+                pile = int(input("Choose Pile: "))
+                count = int(input("Choose Count: "))
+                if (pile, count) in available_actions:
+                    break
+                print("Invalid move, try again.")
+
+        # Have AI make a move
+        else:
+            print("AI's Turn")
+            pile, count = ai.choose_action(game.piles, epsilon=False)
+            print(f"AI chose to take {count} from pile {pile}.")
+
+        # Make move
+        game.move((pile, count))
+
+        # Check for winner
+        if game.winner is not None:
+            print()
+            print("GAME OVER")
+            winner = "Human" if game.winner == human_player else "AI"
+            print(f"Winner is {winner}")
+            return
