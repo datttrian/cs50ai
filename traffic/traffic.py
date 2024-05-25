263442a40586f558e34029badf88a8c21355dc9b
Dat Thanh Tran <datttrian@gmail.com>
2024-05-25 08:31:18 +0000

    Sat, May 25, 2024, 8:31 AM +00:00

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -81,7 +81,8 @@ def get_model():
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT,3 )),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu',
+                               input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
         tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
