9bc3fbaebf193d3df086893446d569a298a654eb
Dat Thanh Tran <datttrian@gmail.com>
2024-05-25 08:25:12 +0000

    Sat, May 25, 2024, 8:25 AM +00:00

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -71,7 +71,7 @@ def load_data(data_dir):
                 images.append(img)
                 labels.append(category)
 
-    return imgaes, labels
+    return images, labels
 
 
 def get_model():
@@ -80,7 +80,30 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT,3 )),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
 
 
 if __name__ == "__main__":
