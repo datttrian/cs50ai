a7d9c0a99e4b8b1e7b05f2f2cb8592e2c74ec936
datttrian <datttrian@gmail.com>
2024-06-25 08:17:06 +0000

    Tue, Jun 25, 2024, 4:17 AM +00:00

--- src0/maze.py
+++ src0/maze.py
@@ -215,16 +215,3 @@ class Maze:
 
         img.save(filename)
 
-
-if len(sys.argv) != 2:
-    sys.exit("Usage: python maze.py maze.txt")
-
-m = Maze(sys.argv[1])
-print("Maze:")
-m.print()
-print("Solving...")
-m.solve()
-print("States Explored:", m.num_explored)
-print("Solution:")
-m.print()
-m.output_image("maze.png", show_explored=True)
