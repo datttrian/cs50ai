2b264a343cac6b8d5caa8259e20cf64ff77b7d0e
datttrian <datttrian@gmail.com>
2024-06-25 08:32:17 +0000

    Tue, Jun 25, 2024, 8:32 AM +00:00

--- src0/maze.py
+++ src0/maze.py
@@ -48,9 +48,9 @@ class Maze:
 
         # Validate start and goal
         if contents.count("A") != 1:
-            raise Exception("maze must have exactly one start point")
+            raise ValueError("maze must have exactly one start point")
         if contents.count("B") != 1:
-            raise Exception("maze must have exactly one goal")
+            raise ValueError("maze must have exactly one goal")
 
         # Determine height and width of maze
         contents = contents.splitlines()
