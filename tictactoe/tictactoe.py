c19f1c9cc970565239d24a89b8f0ebf93b6d6ddb
Dat Thanh Tran <datttrian@gmail.com>
2024-06-26 09:07:38 +0000

    Wed, Jun 26, 2024, 9:07 AM +00:00

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -105,16 +105,22 @@ def minimax(board):
 def max_value(board):
     if terminal(board):
         return utility(board)
+
     v = float("-inf")
+
     for action in actions(board):
         v = max(v, min_value(result(board, action)))
+
     return v
 
 
 def min_value(board):
     if terminal(board):
         return utility(board)
+
     v = float("inf")
+
     for action in actions(board):
         v = min(v, max_value(result(board, action)))
+
     return v
