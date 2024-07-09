9c87b961c87d63f35a20c15f1570d95c0414d4bf
datttrian <datttrian@gmail.com>
2024-07-09 08:02:25 +0000

    Tue, Jul 9, 2024, 8:02 AM +00:00

--- src2/chain/model.py
+++ src2/chain/model.py
@@ -1,18 +1,18 @@
-from pomegranate import *
+from pomegranate import ConditionalProbabilityTable, DiscreteDistribution, MarkovChain
 
 # Define starting probabilities
-start = DiscreteDistribution({
-    "sun": 0.5,
-    "rain": 0.5
-})
+start = DiscreteDistribution({"sun": 0.5, "rain": 0.5})
 
 # Define transition model
-transitions = ConditionalProbabilityTable([
-    ["sun", "sun", 0.8],
-    ["sun", "rain", 0.2],
-    ["rain", "sun", 0.3],
-    ["rain", "rain", 0.7]
-], [start])
+transitions = ConditionalProbabilityTable(
+    [
+        ["sun", "sun", 0.8],
+        ["sun", "rain", 0.2],
+        ["rain", "sun", 0.3],
+        ["rain", "rain", 0.7],
+    ],
+    [start],
+)
 
 # Create Markov chain
 model = MarkovChain([start, transitions])
