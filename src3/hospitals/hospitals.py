e6885c87be055c2f91670004f48c72d0248a3f36
datttrian <datttrian@gmail.com>
2024-07-12 09:02:07 +0000

    Fri, Jul 12, 2024, 9:02 AM +00:00

--- src3/hospitals/hospitals.py
+++ src3/hospitals/hospitals.py
@@ -1,7 +1,9 @@
 import random
 
+from PIL import Image, ImageDraw, ImageFont
 
-class Space():
+
+class Space:
 
     def __init__(self, height, width, num_hospitals):
         """Create a new state space with given dimensions."""
@@ -20,9 +22,7 @@ class Space():
 
         # Consider all possible cells
         candidates = set(
-            (row, col)
-            for row in range(self.height)
-            for col in range(self.width)
+            (row, col) for row in range(self.height) for col in range(self.width)
         )
 
         # Remove all houses and hospitals
@@ -38,7 +38,7 @@ class Space():
 
         # Start by initializing hospitals randomly
         self.hospitals = set()
-        for i in range(self.num_hospitals):
+        for _ in range(self.num_hospitals):
             self.hospitals.add(random.choice(list(self.available_spaces())))
         if log:
             print("Initial state: cost", self.get_cost(self.hospitals))
@@ -75,10 +75,9 @@ class Space():
                 return self.hospitals
 
             # Move to a highest-valued neighbor
-            else:
-                if log:
-                    print(f"Found better neighbor: cost {best_neighbor_cost}")
-                self.hospitals = random.choice(best_neighbors)
+            if log:
+                print(f"Found better neighbor: cost {best_neighbor_cost}")
+            self.hospitals = random.choice(best_neighbors)
 
             # Generate image
             if image_prefix:
@@ -119,12 +118,7 @@ class Space():
 
     def get_neighbors(self, row, col):
         """Returns neighbors not already containing a house or hospital."""
-        candidates = [
-            (row - 1, col),
-            (row + 1, col),
-            (row, col - 1),
-            (row, col + 1)
-        ]
+        candidates = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
         neighbors = []
         for r, c in candidates:
             if (r, c) in self.houses or (r, c) in self.hospitals:
@@ -135,7 +129,6 @@ class Space():
 
     def output_image(self, filename):
         """Generates image with all houses and hospitals."""
-        from PIL import Image, ImageDraw, ImageFont
         cell_size = 100
         cell_border = 2
         cost_size = 40
@@ -144,13 +137,10 @@ class Space():
         # Create a blank canvas
         img = Image.new(
             "RGBA",
-            (self.width * cell_size,
-             self.height * cell_size + cost_size + padding * 2),
-            "white"
+            (self.width * cell_size, self.height * cell_size + cost_size + padding * 2),
+            "white",
         )
-        house = Image.open("assets/images/House.png").resize(
-            (cell_size, cell_size)
-        )
+        house = Image.open("assets/images/House.png").resize((cell_size, cell_size))
         hospital = Image.open("assets/images/Hospital.png").resize(
             (cell_size, cell_size)
         )
@@ -162,10 +152,11 @@ class Space():
 
                 # Draw cell
                 rect = [
-                    (j * cell_size + cell_border,
-                     i * cell_size + cell_border),
-                    ((j + 1) * cell_size - cell_border,
-                     (i + 1) * cell_size - cell_border)
+                    (j * cell_size + cell_border, i * cell_size + cell_border),
+                    (
+                        (j + 1) * cell_size - cell_border,
+                        (i + 1) * cell_size - cell_border,
+                    ),
                 ]
                 draw.rectangle(rect, fill="black")
 
@@ -176,15 +167,19 @@ class Space():
 
         # Add cost
         draw.rectangle(
-            (0, self.height * cell_size, self.width * cell_size,
-             self.height * cell_size + cost_size + padding * 2),
-            "black"
+            (
+                0,
+                self.height * cell_size,
+                self.width * cell_size,
+                self.height * cell_size + cost_size + padding * 2,
+            ),
+            "black",
         )
         draw.text(
             (padding, self.height * cell_size + padding),
             f"Cost: {self.get_cost(self.hospitals)}",
             fill="white",
-            font=font
+            font=font,
         )
 
         img.save(filename)
