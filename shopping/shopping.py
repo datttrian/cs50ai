3b1cc852f6797a2bde74c791d768cbb4ddbb9969
datttrian <datttrian@gmail.com>
2024-07-16 08:30:21 +0000

    Tue, Jul 16, 2024, 8:30 AM +00:00

--- shopping/shopping.py
+++ shopping/shopping.py
@@ -64,8 +64,33 @@ def load_data(filename):
 
     with open(filename, "r") as file:
         reader = csv.reader(file)
+        next(reader)
         for row in reader:
-            print(row)
+            if row[-1] == "TRUE":
+                labels.append(1)
+            else:
+                labels.append(0)
+
+            evidence.append(
+                (
+                    int(row[0]),
+                    float(row[3]),
+                    int(row[4]),
+                    float(row[5]),
+                    float(row[6]),
+                    float(row[7]),
+                    float(row[8]),
+                    float(row[9]),
+                    int(row[11]),
+                    int(row[12]),
+                    int(row[13]),
+                    int(row[14]),
+                    1 if row[15] == "Returning_Visitor" else 0,
+                    1 if row[16] == "TRUE" else 0,
+                )
+            )
+
+    print(evidence)
 
 
 def train_model(evidence, labels):
