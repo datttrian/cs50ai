aa365c6a20768432e0071947481caa98994cd887
datttrian <datttrian@gmail.com>
2024-07-22 09:23:43 +0000

    Mon, Jul 22, 2024, 9:23 AM +00:00

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -1,9 +1,9 @@
-import cv2
-import numpy as np
 import os
 import sys
-import tensorflow as tf
 
+import cv2
+import numpy as np
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 EPOCHS = 10
