2ecaef12966992dce9adc0f4e5048ac9fcddd2e7
datttrian <datttrian@gmail.com>
2024-07-22 08:27:42 +0000

    Mon, Jul 22, 2024, 8:27 AM +00:00

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -1,9 +1,9 @@
-import cv2
-import numpy as np
 import os
 import sys
-import tensorflow as tf
 
+import cv2
+import numpy as np
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 EPOCHS = 10
@@ -35,7 +35,7 @@ def main():
     model.fit(x_train, y_train, epochs=EPOCHS)
 
     # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
+    model.evaluate(x_test, y_test, verbose=2)
 
     # Save model to file
     if len(sys.argv) == 3:
@@ -80,32 +80,27 @@ def get_model():
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
     # Create a convolutional neural network
-    model = tf.keras.models.Sequential([
-
-        # Convolutional layer. Learn 32 filters using a 3x3 kernel
-        tf.keras.layers.Conv2D(
-            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
-        ),
-
-        # Max-pooling layer, using 2x2 pool size
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-
-        # Flatten units
-        tf.keras.layers.Flatten(),
-
-        # Add a hidden layer with dropout
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.5),
-
-        # Add an output layer
-        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
-    ])
+    model = tf.keras.models.Sequential(
+        [
+            # Convolutional layer. Learn 32 filters using a 3x3 kernel
+            tf.keras.layers.Conv2D(
+                32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            ),
+            # Max-pooling layer, using 2x2 pool size
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            # Flatten units
+            tf.keras.layers.Flatten(),
+            # Add a hidden layer with dropout
+            tf.keras.layers.Dense(128, activation="relu"),
+            tf.keras.layers.Dropout(0.5),
+            # Add an output layer
+            tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax"),
+        ]
+    )
 
     # Train neural network
     model.compile(
-        optimizer="adam",
-        loss="categorical_crossentropy",
-        metrics=["accuracy"]
+        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
     )
 
     return model
